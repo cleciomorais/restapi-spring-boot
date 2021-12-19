3c6f364684fcdcd2cb6a73cde7ec6ac5bbb07541
cleciomorais <cleciovmorais@gmail.com>
2021-12-19 17:03:26 -0300

    fix the yml serialization to ignore null properties

--- src/main/java/br/com/restapi/converter/serialization/YamlJackson2HttpMessageConverter.java
+++ src/main/java/br/com/restapi/converter/serialization/YamlJackson2HttpMessageConverter.java
@@ -3,12 +3,15 @@ package br.com.restapi.converter.serialization;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.json.AbstractJackson2HttpMessageConverter;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
 
 public final class YamlJackson2HttpMessageConverter extends AbstractJackson2HttpMessageConverter{
 
 	public YamlJackson2HttpMessageConverter() {
-		super(new YAMLMapper(), MediaType.parseMediaType("application/x-yaml") );
+		super(new YAMLMapper()  
+				//.setSerializationInclusion(JsonInclude.Include.NON_NULL) serve para não serializar as propriedades nulas dos objetos para o yml de retorno
+				.setSerializationInclusion(JsonInclude.Include.NON_NULL) , MediaType.parseMediaType("application/x-yaml") );
 	}
 
 }
